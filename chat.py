a6273367bfc354b44a8126bcbc552204bc33897c
ChobPT <45816945+ChobPT@users.noreply.github.com>
2023-03-23 11:20:03 +0000

    Fixed Capitalization
    bigscience model was not loading due to capitalization.

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import argparse
 parser = argparse.ArgumentParser()
 
-model_map = {'pythia': 'OpenAssistant/oasst-sft-1-pythia-12b', 'bloom': 'BigScience/bloom-7b1', 'gptj': 'EleutherAI/gpt-j-6B'}
+model_map = {'pythia': 'OpenAssistant/oasst-sft-1-pythia-12b', 'bloom': 'bigscience/bloom-7b1', 'gptj': 'EleutherAI/gpt-j-6B'}
 
 parser.add_argument("-p", "--prompt", nargs="+",help="Enter a value for the prompt")
 parser.add_argument("-t", "--tokens",help="Number of tokens to generate",type=int, default=100)
@@ -22,3 +22,4 @@ else:
     my_prompt = ' '.join(args.prompt)
     x = ai.generate("<|prompter|>"+my_prompt+"<|endoftext|><|assistant|>", num_tokens_to_generate=args.tokens)
     print(x['token_str'])
+
