8c28fda96495e592574ec57d44f84a8c8c50beee
ChobPT <45816945+ChobPT@users.noreply.github.com>
2023-03-23 11:08:59 +0000

    More chat like, token and model choice added
    A very simple way to interface with pythia without having to reload the model.
    
    - Added the -m paremeter to chose the model to load, use `-m pythia`, `-m bloom`, or `-m gptj` and it will load them, defaults to pythia
    - Added the -t parameter that will allow you to specify how many tokens
    - Added the -p parameter for you to send an input and get a response for that right away
    - If no -p parameter is present, it'll ask the user to input the prompt until they type 'exit'
    
    
    
    Usage:
    python chat.py -t 200 for 200 tokens
    
    python chat.py -t 200 -p tell me a joke for getting a joke with 200 tokens

---
+++ chat.py
@@ -0,0 +1,24 @@
+import argparse
+parser = argparse.ArgumentParser()
+
+model_map = {'pythia': 'OpenAssistant/oasst-sft-1-pythia-12b', 'bloom': 'BigScience/bloom-7b1', 'gptj': 'EleutherAI/gpt-j-6B'}
+
+parser.add_argument("-p", "--prompt", nargs="+",help="Enter a value for the prompt")
+parser.add_argument("-t", "--tokens",help="Number of tokens to generate",type=int, default=100)
+parser.add_argument("-m", "--model", help="Specify a Model", choices=model_map.keys(),default="pythia")
+args = parser.parse_args()
+
+from interface import AutoInference as AI
+ai = AI(model_map[args.model])
+
+if not args.prompt:
+    while True:
+        name = input("Please enter your prompt (type 'exit' to quit): ")
+        if name.lower() == 'exit':
+            break
+        x = ai.generate("<|prompter|>"+str(name)+"<|endoftext|><|assistant|>", num_tokens_to_generate=args.tokens)
+        print(x['token_str'])
+else:
+    my_prompt = ' '.join(args.prompt)
+    x = ai.generate("<|prompter|>"+my_prompt+"<|endoftext|><|assistant|>", num_tokens_to_generate=args.tokens)
+    print(x['token_str'])
